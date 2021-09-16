be10794437ed1144de8155d82410151de1bb9c0d
Garfield550 <openpgp@550.moe>
2021-09-16 08:26:35 +0000

    feat: add type guard

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+import { MetaWorker } from '@metaio/worker-model';
 import process from 'process';
 
 export const removeControlCharacters = (str: string): string => {
@@ -11,4 +12,19 @@ export const isUndefined = (obj: unknown): obj is undefined =>
 
 export const isProd = (): boolean => {
   return process.env.NODE_ENV === 'production';
+};
+
+export const isDeployTask = (conf: unknown): boolean => {
+  if ((conf as MetaWorker.Configs.DeployTaskConfig).template) return true;
+  return false;
+};
+
+export const isPublishTask = (conf: unknown): boolean => {
+  if ((conf as MetaWorker.Configs.PublishTaskConfig).site) return true;
+  return false;
+};
+
+export const isPostTask = (conf: unknown): boolean => {
+  if ((conf as MetaWorker.Configs.PostTaskConfig).post) return true;
+  return false;
 };
